9a97d77c00e9dffeb234121a0a4f8eb0fe5ad229
ernie-blockswap <94845412+ernie-blockswap@users.noreply.github.com>
2022-12-03 20:33:13 +0300

    add: added working burger-menu

--- script/script.js
+++ script/script.js
@@ -1,7 +1,11 @@
 'use strict';
 //variables
 let burger = document.querySelector('.burger');
+let body = document.querySelector('body');
 
 
 //listeners
-burger.addEventListener('click',() => burger.classList.toggle('active'));
+burger.addEventListener('click',() => {
+    burger.classList.toggle('active');
+    body.classList.toggle('active');
+});
